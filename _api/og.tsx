585973e58e7740ff3fb1bc8b5e1eaf38eccb7176
Ohkubo Kohei <kuboon@trick-with.net>
2024-11-27 16:31:50 +0900

    works

--- _api/og.tsx
+++ _api/og.tsx
@@ -1,26 +1,25 @@
 import { DistributionChartSvg } from '../src/components/DistributionChartSvg';
 import { parseHTML } from 'linkedom';
-import { Resvg, initWasm } from '@resvg/resvg-wasm'
-import resvgWasm from './_resvg.wasm'
+import { initialize, svg2png, } from 'svg2png-wasm';
+import wasm from './_svg2png_wasm_bg.wasm'
 
 export const edge = true;
 
-const initPromise = async () => {
-  await initWasm(resvgWasm)
-  const ttf = await fetch(new URL("./_noto-sans-v27-latin-regular.ttf", import.meta.url))
+const initPromise = initialize(wasm).then(async () => {
+  const ttf = await fetch(new URL("./_roboto-v32-greek_latin-regular.ttf", import.meta.url))
   if (!ttf.ok) throw new Error('font load failed')
   return new Uint8Array(await ttf.arrayBuffer())
-}
+})
 
 export async function GET(request: Request) {
-  const fontBuf = await initPromise()
+  const fontBuf = await initPromise
 
   const { searchParams } = new URL(request.url);
   const lowerBound = parseFloat(searchParams.get('lowerBound') || '-1');
   const upperBound = parseFloat(searchParams.get('upperBound') || '1');
   const width = 1200
   const height = 630
-  const { document } = parseHTML("");
+  const { document } = parseHTML("<body></body>");
   const svgStr = DistributionChartSvg({
     document,
     lowerBound,
@@ -28,22 +27,24 @@ export async function GET(request: Request) {
     width,
     height,
   }).outerHTML;
-  // const svgStr = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 630" width="1200" height="630">
-  //   <rect width="100%" height="100%" fill="blue"/>
-  //   <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="40" fill="black">Hello, World!${request.url}</text>
-  // </svg>`;
 
-  const resvg = new Resvg(svgStr, {
-    fitTo: { mode: 'width', value: 1200 },
-    font: { fontBuffers: [fontBuf], loadSystemFonts: false }
-  })
-  const pngData = resvg.render()
-  const pngBuffer = pngData.asPng()
+  const png = await svg2png(svgStr, {
+    width,
+    height,
+    fonts: [fontBuf],
+    defaultFontFamily: {
+      sansSerifFamily: 'Roboto',
+      serifFamily: 'Roboto',
+      cursiveFamily: 'Roboto',
+      fantasyFamily: 'Roboto',
+      monospaceFamily: 'Roboto',
+    },
+  });
 
-  return new Response(pngBuffer, {
+  return new Response(png, {
     headers: {
       'Content-Type': 'image/png',
-      // 'Cache-Control': 'public, max-age=31536000, immutable',
+      'Cache-Control': 'public, max-age=31536000, immutable',
     },
   });
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,15 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 const dest = '../.vercel/output/functions/api/og.func';
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2022',
+  },
   plugins: [
     vercel(),
     viteStaticCopy({
       targets: [
-        { dest, src: '_api/_resvg.wasm' },
-        { dest, src: '_api/_noto-sans-v27-latin-regular.ttf' }
+        { dest, src: '_api/_*.wasm' },
+        { dest, src: '_api/_*.ttf' }
       ],
     })
   ],
